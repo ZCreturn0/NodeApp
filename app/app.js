1869ea546526d211c7318a3c018cbf2226131b85
zc_return0 <237350543@qq.com>
2019-09-05 14:50:33 +0800

    路由配置

--- app/app.js
+++ app/app.js
@@ -14,16 +14,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'views')));
 
+// 路由
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
